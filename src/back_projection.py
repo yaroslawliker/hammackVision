5b34bc953e7c1932f126039e88339b2031992fc8
Yaroslav Vorobiov <yaroslawliker@gmail.com>
2025-05-14 01:19:43 +0300

    Debugged back_projection.py
    Added classes for better organization

--- src/back_projection.py
+++ src/back_projection.py
@@ -19,12 +19,12 @@ class Camera:
     def get_position(self):
         return self.position
 
-    def self_orientation(self, orientation):
+    def set_orientation(self, orientation):
         self.orientation = np.array(orientation)
     def get_orientation(self):
         return self.orientation
 
-    def get_internal_matrix(self, u, v) -> np.ndarray[3, 3]:
+    def get_internal_matrix(self, u, v):
         K = np.array([
             [self.fx, self.skew, u],
             [0, self.fy, v],
@@ -35,13 +35,13 @@ class Camera:
 
     
     
-class BackProjection:
+class BackProjector:
     def __init__(self, camera: Camera):
         self.camera = camera
     
-    def get_direction_to_point_camera(self, point, u, v):
+    def get_direction_to_point_camera(self, point_pixels, u, v):
         # Convert point to homogeneous coordinates
-        point_homogeneous = np.array([point[0], point[1], 1])
+        point_homogeneous = np.array([point_pixels[0], point_pixels[1], 1])
         
         # Get the camera intrinsic matrix
         K = self.camera.get_internal_matrix(u, v)
@@ -51,18 +51,18 @@ class BackProjection:
         
         return direction_vector
     
-    def get_direction_to_point_world(self, point, u, v):
+    def get_direction_to_point_world(self, point_pixels, u, v):
         # Get the direction vector in camera coordinates
-        direction_camera = self.get_direction_to_point_camera(point, u, v)
+        direction_camera = self.get_direction_to_point_camera(point_pixels, u, v)
         
         # Transform the direction vector to world coordinates
-        direction_world = np.linalg.inv(self.camera.orientation) @ np.linalg.inv(self.camera.position) @ direction_camera
+        direction_world = self.camera.orientation.T @ direction_camera
         
         return direction_world
     
-    def back_project(self, point, u, v):
+    def back_project(self, point_pixels, u, v):
         # Get the direction vector in world coordinates
-        direction_world = self.get_direction_to_point_world(point, u, v)
+        direction_world = self.get_direction_to_point_world(point_pixels, u, v)
         
         t0 = -self.camera.position[1] / direction_world[1]
 
@@ -70,9 +70,109 @@ class BackProjection:
         
         return point_3d
     
+class Photo:
+    def __init__(self, size_pixel_x, size_pixel_y, u = None, v = None):
+        self.size_pixel_x = size_pixel_x
+        self.size_pixel_y = size_pixel_y
+
+        self.u = size_pixel_x / 2 if u == None else u
+        self.v = size_pixel_y / 2 if v == None else v
+
+    def get_u(self):
+        return self.u
+
+    def get_v(self):
+        return self.v
+    
+class PhotoPoint:
+    def __init__(self, pixel_x, pixel_y, photo_settings: Photo):
+        self.photo_settings = photo_settings
+
+        assert pixel_x >= 0 and pixel_x <= photo_settings.size_pixel_x, "pixel_x out of bounds"
+        assert pixel_y >= 0 and pixel_y <= photo_settings.size_pixel_y, "pixel_y out of bounds"
+
+        # Normilize pixel coordinates
+        pixel_x = pixel_x - photo_settings.size_pixel_x
+        pixel_y = pixel_y - photo_settings.size_pixel_y
+
+        self.point_pixels = np.array([pixel_x, pixel_y])
+
+    def get_point_pixels(self):
+        return self.point_pixels
+    
+    
 if __name__ == "__main__":
 
-    pass
+    # In centimeters
+
+    # Camera
+    pixel_size_cm = 0.0018
+    # fx = fy = f / pixel_size_cm = 2.73 / 0.0018 ≈ 1516.67
+    f = 27.3 / 0.0018
+    cameraY = 5.75
+    camera_orientation = np.array([
+        [1, 0, 0],
+        [0, 1, 0],
+        [0, 0, 1]
+    ])
+
+    # Photo settings
+    
+    size_pixel_x = 1280
+    size_pixel_y = 960
+
+    u = size_pixel_x / 2
+    v = size_pixel_y / 2
+
+    galaxy50ACamera = Camera(f, f)
+    
+    galaxy50ACamera.set_position([0, cameraY, 0])
+    galaxy50ACamera.set_orientation(camera_orientation)
+
+    back_projection = BackProjector(galaxy50ACamera)
+
+    # Photo (cola1)
+    pixel_x = size_pixel_x - 742
+    pixel_y = size_pixel_y - 600
+    # size_pixel_y - point_pixels[1]
+    point = np.array([pixel_x, pixel_y])
+
+    point_3d = back_projection.back_project(point, u, v)
+    print("3D point in world coordinates:", point_3d)
+
+    # Photo (cola1)
+    pixel_x = size_pixel_x - 1080
+    pixel_y = size_pixel_y - 672
+    point = np.array([pixel_x, pixel_y])
+
+    point_3d = back_projection.back_project(point, u, v)
+    print("3D point in world coordinates:", point_3d)
+
+
+    back_projection = BackProjector(galaxy50ACamera)
+
+    # Photo (cola1)
+    size_pixel_x = 800
+    size_pixel_y = 533
+    u = size_pixel_x / 2
+    v = size_pixel_y / 2
+    pixel_x = size_pixel_x - 465
+    pixel_y = size_pixel_y - 336
+    # size_pixel_y - point_pixels[1]
+    point = np.array([pixel_x, pixel_y])
+
+    point_3d = back_projection.back_project(point, u, v)
+    print("3D point in world coordinates:", point_3d / 100)
+
+    pixel_x = size_pixel_x - 638
+    pixel_y = size_pixel_y - 333
+    # size_pixel_y - point_pixels[1]
+    point = np.array([pixel_x, pixel_y])
+
+    point_3d = back_projection.back_project(point, u, v)
+    print("3D point in world coordinates:", point_3d / 100)
+
+    
 
 
     
