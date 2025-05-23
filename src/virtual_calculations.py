d5b51e463889dd28a8d15edde7d2f61926832776
Yaroslav Vorobiov <yaroslawliker@gmail.com>
2025-05-23 15:33:20 +0300

    Added multiple points to virtual_calculations.py

--- src/virtual_calculations.py
+++ src/virtual_calculations.py
@@ -4,13 +4,17 @@ from scipy.spatial.transform import Rotation
 import back_projection as bp
 
 def back_project_virtual(
-        photo_size: tuple, 
+        photo_size: np.ndarray, 
         horisontal_camera_size,
         f,
         camera_y,
-        camera_euler_rotation_agles: tuple,
-        point_pixels: tuple
+        camera_euler_rotation_agles: np.ndarray,
+        points_pixels: np.ndarray
     ) -> np.array:
+
+    assert len(photo_size) == 2, "photo_size must be a 2D array"
+    assert len(camera_euler_rotation_agles) == 3, "camera_euler_rotation_agles must be a 3D array"
+    assert points_pixels.shape[0] > 0 and points_pixels.shape[1] == 2, "points_pixels must be an array of 3D arrays, N > 0"
     
     photo_width = photo_size[0]
     photo_height = photo_size[1]
@@ -32,28 +36,44 @@ def back_project_virtual(
     # Instantiate the back projector
     projector = bp.BackProjector(camera)
     
-    # Create a photo settings
     photo = bp.Photo(photo_width, photo_height)
 
-    # Create the green point
-    green_point = bp.PhotoPoint(669, 685, photo)
+    points_3D = []
 
-    point_3D = projector.back_project(green_point)
-    return point_3D
+    # Create a photo settings
+    for i, point in enumerate(points_pixels):
+        # Create the photo point
+        photo_point = bp.PhotoPoint(point[0], point[1], photo)
+        point_3D = projector.back_project(photo_point)
+        points_3D.append(point_3D)
+
+    return np.array(points_3D)
+
+def print_3D_points(points_3D):
+    print("3D point coordinates (cm):")
+    for i, point in enumerate(points_3D):
+        print(f"\tPoint {i}: {point}")
     
 
 def virtual_straight():
-    point_3D = back_project_virtual(
-        photo_size=(1920, 1080),
+    points_3D = back_project_virtual(
+        photo_size=np.asarray([1920, 1080]),
         horisontal_camera_size=36 / 100,
         f=30 / 100,
         camera_y=100,
-        camera_euler_rotation_agles=(0, 0, 0),
-        point_pixels=(669, 685)
+        camera_euler_rotation_agles=np.array([0, 0, 0]),
+        points_pixels=np.array([
+            [669, 685],
+
+            [827, 806],
+            [964, 805],
+            [1092, 807],
+            
+            [1416, 654]
+            ])
     )
-    print("3D point coordinates (cm):", point_3D)
 
-
+    print_3D_points(points_3D)
 
 if __name__ == "__main__":
     virtual_straight()
