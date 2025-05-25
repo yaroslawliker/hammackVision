d42ac76f85e8826beb101f5bdc0da57ce5a8f532
Yaroslav Vorobiov <yaroslawliker@gmail.com>
2025-05-25 18:35:42 +0300

    Moved calculations inputs to json

--- src/json_calculations.py
+++ src/json_calculations.py
@@ -24,9 +24,19 @@ def run_calculation(data: list, camera_names: list = None, comparing=False):
 
             if (comparing == True):
                 print("Errors:")
-                points_actual = [point["real"] for point in points]
+                points_actual = []
+                for point in points:
+                    try:
+                        points_actual.append(point["real"])
+                    except KeyError:
+                        points_actual.append(None)                    
+
                 i = 0
                 for point_actual, point_estimated in zip(points_actual, points_3D):
+                    if (point_actual is None):
+                        print(f"\tPoint {i}: no real calculation")
+                        continue
+
                     error_abs = [point_actual[i] - point_estimated[i] for i in range(3)]
                     error_rel = [error_abs[i] / point_estimated[i] for i in (0,2)]
                     error_rel.insert(1, 0)
@@ -40,11 +50,26 @@ def run_calculation(data: list, camera_names: list = None, comparing=False):
 
                     i+=1
 
-if __name__ == "__main__":
-    json_path = "data/test/back projection/virtual.json"
-    session_names = ["15degree"]
-
+def run_calculation_on_path(json_path, session_names = None):
     with open(json_path, "rt") as file:
         parsed = json.load(file)
-        
         run_calculation(parsed, session_names, True)
+
+if __name__ == "__main__":
+
+    run_calculation_on_path(
+        "data/test/back projection/virtual/3cubes.json",
+        ["straight","15degree"]
+    )
+
+    run_calculation_on_path(
+        "data/test/back projection/real/noinfo.json",
+        ["random city photo"]
+    )
+
+    run_calculation_on_path(
+        "data/test/back projection/real/room.json",
+        ["table", "fridge"]
+    )
+
+    
